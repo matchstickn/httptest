487d216a08908f549d24007cea29f96038f58fbb
matchstickn <141876523+matchstickn@users.noreply.github.com>
2024-05-12 17:55:34 -0400

    Create middle.go

---
+++ middle/middle.go
@@ -0,0 +1,13 @@
+package middle
+
+import (
+	"log"
+	"net/http"
+)
+
+func Logger(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path)
+		f(w, r)
+	}
+}
