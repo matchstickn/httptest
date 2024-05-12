1b87e8c7f28939f04f64c64193d9ad89882176fd
matchstickn <141876523+matchstickn@users.noreply.github.com>
2024-05-12 17:55:20 -0400

    Update main.go

--- main.go
+++ main.go
@@ -4,33 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"pushtest/middle"
 	"pushtest/rps"
 )
 
 func main() {
 	router := http.NewServeMux()
 
-	router.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "anything")
-	})
-
-	router.HandleFunc("GET /api", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "getting")
-	})
-
-	router.HandleFunc("GET /api/test", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/api/test", middle.Logger(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "getting test")
-	})
+	}))
 
-	router.HandleFunc("POST /api", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "posting")
-	})
-
-	router.HandleFunc("/api/{item}/", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/api/{item}/", middle.Logger(func(w http.ResponseWriter, r *http.Request) {
 		item := r.PathValue("item")
 		outcome := rps.Play(item)
 		fmt.Fprint(w, outcome)
-	})
+	}))
 
 	port := ":4000"
 
