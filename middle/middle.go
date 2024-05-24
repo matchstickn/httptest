8c04cc3d8bb694fa31c8157fd2b5b036d8ef25bd
matchstickn <141876523+matchstickn@users.noreply.github.com>
2024-05-23 22:47:24 -0400

    Update middle.go

--- middle/middle.go
+++ middle/middle.go
@@ -9,8 +9,14 @@ import (
 func Logger(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		//request_first_called_time := time.Now()
-		f(w, r)
+		f.ServeHTTP(w, r)
 		//time_elapsed := time.Since(request_first_called_time)
-		log.Println(r.URL.Path, "   ")//, time_elapsed.Milliseconds(), "ms")
+		log.Println(r.URL.Path, "   ") //, time_elapsed.Milliseconds(), "ms")
 	}
+}
+
+func CORS(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
