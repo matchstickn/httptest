75dfaa47d07a4f7ebc16bb0c6f149b03742210b8
matchstickn <141876523+matchstickn@users.noreply.github.com>
2024-06-02 20:49:12 -0400

    Update middle.go

--- middle/middle.go
+++ middle/middle.go
@@ -3,15 +3,12 @@ package middle
 import (
 	"log"
 	"net/http"
-	//"time"
 )
 
 func Logger(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//request_first_called_time := time.Now()
 		f.ServeHTTP(w, r)
-		//time_elapsed := time.Since(request_first_called_time)
-		log.Println(r.URL.Path, "   ") //, time_elapsed.Milliseconds(), "ms")
+		log.Println(r.URL.Path, "   ")
 	}
 }
 
